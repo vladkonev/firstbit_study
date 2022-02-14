f0b2afbd316587e09d33cae0d561accf3e6e84e5
Vlad Konev <threefiftyzet@yandex.ru>
2022-02-14 19:21:56 +0300

    add 1 more if in php file

--- src/php/backend.php
+++ src/php/backend.php
@@ -15,11 +15,11 @@
             <div class="login__title">Пожалуйста авторизуйтесь</div>
                 <form action="" method="POST" class="login__form">
                         <div class="login__input login__name">
-                        <input placeholder="example" name="name" type="text" required>
+                        <input placeholder="example" name="name" type="text">
                             <label for="name">Логин</label>  
                         </div>
                         <div class="login__input login__password">
-                        <input placeholder="*******" name="password" type="text" required>
+                        <input placeholder="*******" name="password" type="text">
                             <label for="password">Пароль</label>
                         </div>
                         <button class="btn">Авторизоваться</button>
@@ -36,6 +36,8 @@
         const PASS = '245';
     }
 
+
+
     
     if($_POST['name'] == User::NAME && $_POST['password'] == User::PASS) {
         echo '<h2 class="login__error">' . 'Вы авторизованы ' . $_POST['name'] . '</h2>';
@@ -47,6 +49,10 @@
 
     if($_POST['name'] == User::NAME && $_POST['password'] !== User::PASS) {
         echo '<link rel="stylesheet" href="../sass/blocks/_link_pass.css">' . '<h2 class="login__error">' . 'Не правильно введён пароль' . '</h2>' ;
+    }
+
+    if($_POST['name'] !== User::NAME && $_POST['password'] !== User::PASS) {
+        echo '<link rel="stylesheet" href="../sass/blocks/_link_name.css">' . '<h2 class="login__error">' . 'Пользователь не найден' . '</h2>' ;
     }
 
 ?>
